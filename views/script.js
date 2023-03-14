e8c94506ee09f051d1e5882186cf739514507c54
jmckelvey786 <77301869+jmckelvey786@users.noreply.github.com>
2023-03-13 21:57:11 -0400

    linked modals to add food buttons

--- views/script.js
+++ views/script.js
@@ -5,6 +5,57 @@ let year = todayDate.getFullYear();
 let formattedDate = `${month}-${day}-${year}`;
 const dateElement = document.getElementById('date')
 
+const bModal = document.getElementById('breakfast-modal');
+const breakfastButton = document.getElementById('add-breakfast');
+let bspan = document.getElementById("bclose");
+
+const lModal = document.getElementById('lunch-modal');
+const lunchButton = document.getElementById('add-lunch');
+let lspan = document.getElementById("lclose");
+
+const dModal = document.getElementById('dinner-modal');
+const dinnerButton = document.getElementById('add-dinner');
+let dspan = document.getElementById("dclose");
+
+const sModal = document.getElementById('snack-modal');
+const snackButton = document.getElementById('add-snack');
+let sspan = document.getElementById("sclose");
+
+
+
+
+
 dateElement.innerHTML = formattedDate;
+
+
+breakfastButton.onclick = function() {
+  bModal.style.display = "block";
+}
+bspan.onclick = function() {
+  bModal.style.display = "none";
+}
+
+lunchButton.onclick = function() {
+  lModal.style.display = "block";
+}
+lspan.onclick = function() {
+  lModal.style.display = "none";
+}
+
+dinnerButton.onclick = function() {
+  dModal.style.display = "block";
+}
+dspan.onclick = function() {
+  dModal.style.display = "none";
+}
+
+snackButton.onclick = function() {
+  sModal.style.display = "block";
+}
+sspan.onclick = function() {
+  sModal.style.display = "none";
+}
+
+
 
 
