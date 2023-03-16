4e1520a1608c78263ed81bdf951c593db4a05e5c
Wanamaker919 <116662904+Wanamaker919@users.noreply.github.com>
2023-03-15 21:44:33 -0400

    the charts are working

--- public/js/acquisitions.js
+++ public/js/acquisitions.js
@@ -19,3 +19,23 @@ new Chart(ctx, {
     }
   }
 });
+
+const waterChart = document.getElementById('myChart2');         
+new Chart(waterChart, {
+  type: 'bar',
+  data: {
+    labels: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+    datasets: [{
+      label: 'Macro Breakdown',
+      data: [2, 5, 8, 5, 7, 11, 3],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
